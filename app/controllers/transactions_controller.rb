d6a5ecfdf98c1a88444cacbfe3814d7a296a0197
joaquin <jecorrea@protonmail.com>
2022-03-01 14:52:42 -0500

    controller index method added

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,19 @@
 class TransactionsController < ApplicationController
+
+    def index 
+        #Variables
+        transactions = Transaction.all
+        recent_transactions = {}
+        #Iterate through and take out the proper attributes from all transactions
+        transactions.map do |transaction|
+            recent_transactions.key?(transaction.payer) ? recent_transactions[transaction.payer] += transaction.points : recent_transactions[transaction.payer] = transaction.points
+        end    
+        render json: recent_transactions
+    end
+
+    private
+
+    def transaction_params
+        params.permit(:payer, :points, :timestamp)
+    end
 end
