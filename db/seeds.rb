4aae955cc5a1efef8f751be01d3f987eb065f05d
joaquin <jecorrea@protonmail.com>
2022-03-01 14:36:14 -0500

    added data to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+tran1 = Transaction.create({ "payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z" })
+tran2 =  Transaction.create({ "payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z" })
+tran3 =  Transaction.create({ "payer": "DANNON", "points": -200, "timestamp": "2020-10-31T15:00:00Z" })
+tran4 = Transaction.create({ "payer": "MILLER COORS", "points": 10000, "timestamp": "2020-11-01T14:00:00Z" })
+tran5 = Transaction.create({ "payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z" })
